3cbf23becf404a25ab785b817ec90b0faa8ba8ec
Oleksandr Palamarchuk <alex.y.palamarchuk@gmail.com>
2020-11-27 12:47:16 +0200

    init lesson3

---
+++ bootlesson3/src/main/java/com/itvdn/Bootlesson3Application.java
@@ -0,0 +1,12 @@
+package com.itvdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootlesson3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootlesson3Application.class, args);
+    }
+}
---
+++ bootlesson3/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello, ${name}!</title>
+        <link href="/css/main" rel="stylesheet">
+    </head>
+
+    <body>
+        <h2 class="hello-title">Hello, ${name}</h2>
+        <script src="/js/main.js"/>
+    </body>
+</html>
